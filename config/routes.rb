ac42c1624a6f339e5bbf87b23c881e9d2342050f
Karyatis <anna.p212@gmail.com>
2020-10-25 13:00:04 -0500

    Add api controller and prep routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  MarketPlaceApi::Application.routes.draw do
+    # Api definition
+    namespace :api do
+    # We are going to list our resources here
+    end
+  end
 end
